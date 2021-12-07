065c9f22691c9d164178e6b2eb89902414b1aad2
Salman S. Rahman <salmansr1995@gmail.com>
2021-12-07 12:15:27 +0600

    first commit

---
+++ onclick.js
@@ -0,0 +1,35 @@
+function message1() {
+    alert("Mohib Jan")
+}
+function message2() {
+    alert("Asghar Jan")
+}
+function message3() {
+    alert("Zabi Jan")
+}
+
+// =========== message part end =========
+
+var para = document.querySelector("#para");
+
+function para1() {
+    para.innerHTML = "This is awsome paragraph one"
+}
+
+function para2() {
+    para.innerHTML = "This is wonderful paragraph two"
+}
+
+// ========= paragraph part end ==========
+
+
+var imgId = document.querySelector("#img_id");
+function image1() {
+    imgId.src = "images/1.jpeg"
+}
+function image2() {
+    imgId.src = "images/2.jpg"
+}
+
+
+
